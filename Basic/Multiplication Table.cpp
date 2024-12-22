798053a8b7c0dcaee7ac65c51719f72d2217b6c4
Karan <karanmehtawork9@gmail.com>
2024-12-22 18:25:09 +0530

    input of number till u want to print the table

--- Basic/Multiplication Table.cpp
+++ Basic/Multiplication Table.cpp
@@ -3,11 +3,14 @@ using namespace std;
 
 int main(){
     int n;
+    int f;
 
     cout<<"enter a number to print multiplication ";
     cin>>n;
+    cout<<"enter the number till you wnat to print the table multiplication";
+    cin>>f;
 
-    for(int i = 1;i<=10;i++){
+    for(int i = 1;i<=f;i++){
         cout<<n<<"*"<<i<<"="<<n*i<<endl;;
     }
     return 0;
