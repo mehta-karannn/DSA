b9970b8de79d313f8eb11b4b56326fb509d7284f
Karan <karanmehtawork9@gmail.com>
2024-12-22 13:58:09 +0530

    sum of 2 numbers

---
+++ Basic/SumOfTwoNumbers.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    int b ;
+    cout<<"enter two numbers:";
+    cin>>a>>b;
+    int sum = a + b ;
+    
+    cout << "The Sum of 2 numbers are :"<<sum<<endl;
+
+    return 0;
+
+
+}
