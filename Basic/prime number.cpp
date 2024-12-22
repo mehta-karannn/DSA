16cd8f3057b82cacd6119f7f61c7438d4e9f46da
Karan <karanmehtawork9@gmail.com>
2024-12-22 14:42:59 +0530

    Prime NUmber Checking

--- Basic/prime number.cpp
+++ Basic/prime number.cpp
@@ -7,7 +7,7 @@ int main(){
     cin>>n;
 
     if(n<2){
-        cout<<n<<"It is not a prime number";
+        cout<<"It is not a prime number";
     }else {
         for(int i =2 ; i<n ;i++){
             if(n%i==0){
