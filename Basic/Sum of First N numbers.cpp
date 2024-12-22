6a1f5cbeeb0c5718d04c81eb20ed163119060806
Karan <karanmehtawork9@gmail.com>
2024-12-22 15:47:21 +0530

    sum of first n numbers

---
+++ Basic/Sum of First N numbers.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n ;
+    cout<<"Enter the positive numbers: ";
+    cin>>n;
+
+    int sum =(n*(n+1)/2);
+    cout<<"The sum of First "<< n <<" numbers is "<<sum <<endl;
+    return 0;
+}
