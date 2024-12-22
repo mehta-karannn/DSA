9e8f949c0402429715e74528e9e9c4db0f84acf1
Karan <karanmehtawork9@gmail.com>
2024-12-22 14:50:21 +0530

    Multiplication of Number

---
+++ Basic/Multiplication Table.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+
+    cout<<"enter a number to print multiplication ";
+    cin>>n;
+
+    for(int i = 1;i<=10;i++){
+        cout<<n<<"*"<<i<<"="<<n*i<<endl;;
+    }
+    return 0;
+
+
+}
