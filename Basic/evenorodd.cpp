99cf2f3465e3abb598f9f961a9d6af146127dcb9
Karan <karanmehtawork9@gmail.com>
2024-12-22 14:02:31 +0530

    even or odd

---
+++ Basic/evenorodd.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a ;
+    cout<<"Even Or Odd?:";
+    cin>>a;
+    if(a%2==0){
+        cout<<"the number is even";
+    }else{
+        cout<<"The Number is odd";
+    }
+    return 0;
+}
