ddd50e60fe58c2b1e04c41857ac43ab730ad48a5
Karan <karanmehtawork9@gmail.com>
2024-12-25 21:45:28 +0530

    Identifying the size of an vector

---
+++ Array/vector/Size of Array.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> size={7,9,3,1,4,7};  //-->Creating an Vector
+
+    //Finding out the size of an vector
+    int v = size.size();
+    cout<<v;
+    
+
+}
