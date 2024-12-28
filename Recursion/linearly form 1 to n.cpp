254ba1d2e82f25d65caf0964c60abf7ec267c304
Karan <karanmehtawork9@gmail.com>
2024-12-28 21:58:04 +0530

    1 to n using recurion

---
+++ Recursion/linearly form 1 to n.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void name (int i , int n){
+    if(i>n) return;
+
+    cout<<i<<endl;
+
+    name(i+1,n);
+
+
+}
+
+int main(){
+    int n ;
+    cout<<"enter a number to print your name";
+    cin>>n;
+    name(1,n);
+}
